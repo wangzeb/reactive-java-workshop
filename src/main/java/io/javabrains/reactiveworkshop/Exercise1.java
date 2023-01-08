76fd6f75984bba798c5c4ba068fc7d5c5d831d7b
Zebin Wang <zebin.fan@gmail.com>
2023-01-08 10:15:57 -0500

    reactive test

--- src/main/java/io/javabrains/reactiveworkshop/Exercise1.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise1.java
@@ -7,23 +7,26 @@ public class Exercise1 {
         // Use StreamSources.intNumbersStream() and StreamSources.userStream()
 
         // Print all numbers in the intNumbersStream stream
-        // TODO: Write code here
+        //
 
         // Print numbers from intNumbersStream that are less than 5
-        // TODO: Write code here
+        // StreamSources.intNumbersStream().filter(i -> i < 5).forEach(i -> System.out.println(i));
 
         // Print the second and third numbers in intNumbersStream that's greater than 5
-        // TODO: Write code here
+        //StreamSources.intNumbersStream().limit(3).skip(1).filter(i -> i > 5).forEach(i -> System.out.println(i));
+        StreamSources.intNumbersStream().filter(i -> i > 5).skip(1).limit(2).forEach(System.out::println);
 
         //  Print the first number in intNumbersStream that's greater than 5.
         //  If nothing is found, print -1
-        // TODO: Write code here
+        // System.out.println(StreamSources.intNumbersStream().filter(i -> i > 5).findFirst().orElse(-1));
 
         // Print first names of all users in userStream
-        // TODO: Write code here
+        // StreamSources.userStream().forEach(u -> System.out.println(u.getFirstName()));
 
         // Print first names in userStream for users that have IDs from number stream
-        // TODO: Write code here
+//        StreamSources.userStream()
+//                .filter(u -> StreamSources.intNumbersStream().anyMatch(i -> i == u.getId()))
+//                .forEach(u -> System.out.println(u.getFirstName()));
 
     }
 
--- src/main/java/io/javabrains/reactiveworkshop/Exercise3.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise3.java
@@ -1,6 +1,7 @@
 package io.javabrains.reactiveworkshop;
 
 import java.io.IOException;
+import java.util.List;
 
 public class Exercise3 {
 
@@ -10,11 +11,12 @@ public class Exercise3 {
 
         // Get all numbers in the ReactiveSources.intNumbersFlux stream
         // into a List and print the list and its size
-        // TODO: Write code here
+        List<Integer> s = ReactiveSources.intNumbersFlux().toStream().toList();
+        s.forEach(e -> System.out.println(e));
 
+        System.out.println(s.size());
 
-        System.out.println("Press a key to end");
-        System.in.read();
+       
     }
 
 }
